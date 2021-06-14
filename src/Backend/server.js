3674ee814a4d36c388341fd0aaab958c07721c3a
JohannaForsberg93 <johanna_forsberg@msn.com>
2021-06-14 19:04:24 +0200

    testar igen

--- src/Backend/server.js
+++ src/Backend/server.js
@@ -33,10 +33,10 @@ app.use('/login', require('../Routes/userRoute'));
 //Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
 	// Set static folder
-	app.use(express.static('client/build'));
+	app.use(express.static('yummibook/build'));
 
 	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+		res.sendFile(path.resolve(__dirname, 'yummibook', 'build', 'index.html'));
 	})
 }
 
